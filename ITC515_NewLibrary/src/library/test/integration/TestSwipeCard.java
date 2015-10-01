9c62510498bab591678a0d98a1abbfb9820949bd
samuel mcgann <sam_is_123_789@hotmail.com>
2015-10-01 21:21:52 +1000

    updated TestSwipeCard

--- ITC515_NewLibrary/src/library/test/integration/TestSwipeCard.java
+++ ITC515_NewLibrary/src/library/test/integration/TestSwipeCard.java
@@ -13,9 +13,6 @@ import library.daos.LoanDAO;
 import library.daos.LoanHelper;
 import library.daos.MemberHelper;
 import library.daos.MemberDAO;
-import library.entities.Book;
-import library.entities.Loan;
-import library.entities.Member;
 import library.hardware.*;
 import library.hardware.Scanner;
 import library.interfaces.EBorrowState;
@@ -52,39 +49,25 @@ public class TestSwipeCard {
 	ILoanHelper loanHelper = new LoanHelper();
 	IMemberHelper memberHelper = new MemberHelper();
 	ABorrowPanel ui;
-	String firstName;
-	String lastName;
-	String email;
-	String contactPhone;
-	String author;
-	String title;
-	String callNo;
-	Calendar cal;
-	Date borrowDate;
-	Date dueDate;
+	
 	
 	
 	
 	@Before
 	public void setUp() throws Exception {
-		reader = new CardReader();
-		scanner = new Scanner();
-		printer = new Printer();
-		display = new Display();
+		reader = mock(ICardReader.class);
+		scanner = mock(IScanner.class);
+		printer = mock(IPrinter.class);
+		display = mock(IDisplay.class);
 		bookDao = new BookDAO(bookHelper);
 		loanDao = new LoanDAO(loanHelper);
-		memberDao = new MemberDAO(memberHelper);
+		memberDao = mock(IMemberDAO.class);
 		//helper = mock(IMemberHelper.class);
 		ui = mock(ABorrowPanel.class);
 		//reader.addListener(ctl);
 		ctl = new BorrowUC_CTL(reader, scanner, printer, display, 
 				bookDao, loanDao, memberDao, ui);
 		ctl.initialise();
-		cal = Calendar.getInstance();;
-		borrowDate = cal.getTime();
-		cal.setTime(borrowDate);
-		cal.add(Calendar.DATE, ILoan.LOAN_PERIOD);
-		dueDate = cal.getTime();
 	}
 
 	@After
@@ -102,51 +85,47 @@ public class TestSwipeCard {
 
 	@Test
 	public void testSwipeCard() {
-		IMember member = new Member(firstName, lastName, contactPhone, email, 1);
-		IBook book = new Book(author,title,callNo,1);
-		ILoan loan = new Loan(book, member, borrowDate, dueDate);
+		
+		IMember member = mock(IMember.class);
+		ILoan loan = mock(ILoan.class);
+		List<ILoan> mockList = mock(List.class);
+		Iterator<ILoan> mockIterator = mock(Iterator.class);
+		int id = 1;
 		
 		
+		when(mockList.iterator()).thenReturn(mockIterator);
+		when(mockIterator.hasNext()).thenReturn(true, false);
+		when(mockIterator.next()).thenReturn(loan);
+		when(memberDao.getMemberByID(id)).thenReturn(member);
+		when(member.hasOverDueLoans()).thenReturn(false);
+		when(member.hasReachedLoanLimit()).thenReturn(false);
+		when(member.hasFinesPayable()).thenReturn(false);
+		when(member.hasReachedFineLimit()).thenReturn(false);
+		when(member.getLoans()).thenReturn(mockList);
 		
+		when(loan.toString()).thenReturn("loanDetails");
 		
-//		List<ILoan> mockList = mock(List.class);
-//		Iterator<ILoan> mockIterator = mock(Iterator.class);
-//		int id = 1;
-//		
-//		
-//		when(mockList.iterator()).thenReturn(mockIterator);
-//		when(mockIterator.hasNext()).thenReturn(true, false);
-//		when(mockIterator.next()).thenReturn(loan);
-//		when(memberDao.getMemberByID(id)).thenReturn(member);
-//		when(member.hasOverDueLoans()).thenReturn(false);
-//		when(member.hasReachedLoanLimit()).thenReturn(false);
-//		when(member.hasFinesPayable()).thenReturn(false);
-//		when(member.hasReachedFineLimit()).thenReturn(false);
-//		when(member.getLoans()).thenReturn(mockList);
-//		
-//		when(loan.toString()).thenReturn("loanDetails");
-//		
-//		when(display.getDisplay()).thenReturn(null);
-//		
-//		ctl.cardSwiped(id);
-//		
-//		verify(memberDao).getMemberByID(id);
-//		verify(member).hasOverDueLoans();
-//		verify(member).hasReachedLoanLimit();
-//		verify(member).hasFinesPayable();
-//		verify(member).hasReachedFineLimit();
-//		EBorrowState actualState = ctl.getState();
-//		assertEquals(EBorrowState.SCANNING_BOOKS, actualState);
-//		
-//		verify(reader).setEnabled(false);
-//		//verify(scanner).setEnabled(true);
-//		verify(ui).setState(EBorrowState.SCANNING_BOOKS);
-//		verify(ui).displayScannedBookDetails("");
-//		verify(ui).displayPendingLoan("");
-//		
-//	
-//		verify(ui).displayExistingLoan("loanDetails");
-//		
+		when(display.getDisplay()).thenReturn(null);
+		
+		ctl.cardSwiped(id);
+		
+		verify(memberDao).getMemberByID(id);
+		verify(member).hasOverDueLoans();
+		verify(member).hasReachedLoanLimit();
+		verify(member).hasFinesPayable();
+		verify(member).hasReachedFineLimit();
+		EBorrowState actualState = ctl.getState();
+		assertEquals(EBorrowState.SCANNING_BOOKS, actualState);
+		
+		verify(reader).setEnabled(false);
+	
+		verify(ui).setState(EBorrowState.SCANNING_BOOKS);
+		verify(ui).displayScannedBookDetails("");
+		verify(ui).displayPendingLoan("");
+		
+	
+		verify(ui).displayExistingLoan("loanDetails");
+		
 		
 		
 		
